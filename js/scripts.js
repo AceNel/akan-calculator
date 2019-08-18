f548bc172e7d7eeebc1c0ead9312e7566827fd1e
Nelson Kimani <nelsonkimani96@gmail.com>
2019-08-18 22:16:15 +0300

    Add a days of the week array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 function MakeArray(n) {
   for (var i = 0; i <= n; i++) {}
 }
+days = new MakeArray(7);
+days[0] = "Saturday"
+days[1] = "Sunday"
+days[2] = "Monday"
+days[3] = "Tuesday"
+days[4] = "Wednesday"
+days[5] = "Thursday"
+days[6] = "Friday"
